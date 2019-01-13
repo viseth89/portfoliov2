ee302e3005baa0a05aeff4769ce35c9f312a61c3
viseth89 <viseth89@gmail.com>
2019-01-12 20:47:25 -0600

    added authorization to create, hid edit/delete buttons unless logged in

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin 
 from django.views.generic import ListView, DetailView 
 from django.views.generic.edit import UpdateView, DeleteView, CreateView 
 from django.urls import reverse_lazy 
@@ -8,21 +9,30 @@ class BlogListView(ListView):
     model=Blog
     template_name ='blog/blog_list.html'
 
+
 class BlogDetailView(DetailView):
     model=Blog
     template_name ='blog/blog_detail.html'
+    login_url = 'login' 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model=Blog
     fields = ('title', 'body',)
     template_name ='blog/blog_edit.html'
+    login_url = 'login' 
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
     model=Blog
     template_name ='blog/blog_delete.html'
     success_url = reverse_lazy('blog_list')
+    login_url = 'login' 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model=Blog
     template_name='blog/blog_new.html'
-    fields=('title', 'body', 'author')
+    fields=('title', 'body')
+    login_url = 'login' 
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
